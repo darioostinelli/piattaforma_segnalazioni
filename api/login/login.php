34ae0c23b41cc5f8869fb7c5b4ba875872e0270b
darioostinelli <daostinelli@gmail.com>
2017-10-04 19:10:03 +0200

    controllo login lato server
    modifica file /api/login/login.php per il controllo della password e dell'esistenza dell'utente

--- api/login/login.php
+++ api/login/login.php
@@ -1,3 +1,36 @@
 <?php
+    include_once '../../util/connessioneDB.php';
+    session_start();
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        header('Location /index.html');
+        die();
+    }
+    if(!isset($_POST['login'])){
+        header('Location /index.html');
+        die();
+    }
+    $data = json_decode($_POST['login']);
+    $user = $data->user;
+    $pass = $data->pass;
+    controllaLogin($user, $pass);
     
+    function controllaLogin($user, $pass){
+        $db = new Database();
+        $conn = $db->getConnection();
+        $sql = 'SELECT * FROM users WHERE user_name="'.$user.'";';
+        $result = $conn->query($sql);
+        if($result->num_rows <= 0){ //utente non trovato
+            echo '{"status":"error","error":"user non trovato"}';
+        }
+        else{
+            $obj = $result->fetch_object();
+            if($obj->pass == $pass){
+                 echo '{"status":"success"}';
+            }
+            else{
+                 echo '{"status":"error","error":"password errata"}';
+            }
+        }
+        $conn->close();
+    }
 ?>
--- js/componenti/loginTemplate.js
+++ js/componenti/loginTemplate.js
@@ -5,12 +5,27 @@ function toggleLogin(){
 
 function login(){
     loginError();
-    if($('#username').val() == '')
+    var user = $('#username').val();
+    var pass = $('#password').val();
+    if(user === '')
         loginError('Inserire nome utente');
-    else if($('#password').val() == '')
+    else if(pass === '')
         loginError('Inserire password');
+    else{
+        performLogin(user, pass);
+    }
 }
 
 function loginError(error = ''){
     $('#loginInfo').text(error);
+}
+
+function performLogin(user, pass){
+    var data = {user: user, pass: pass};
+    var jsonData = JSON.stringify(data);
+    $.post('api/login/login.php',
+           {login: jsonData},
+           function(data){
+                
+            });
 }
