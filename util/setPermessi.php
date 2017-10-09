67b9fe03ded2db68cfdea396af0451a7b924765a
darioostinelli <daostinelli@gmail.com>
2017-10-09 17:04:34 +0200

    aggiunto nuovo permesso

--- util/setPermessi.php
+++ util/setPermessi.php
@@ -2,8 +2,12 @@
     include_once '../../util/getPermessi.php';
     class ScriviPermessi{
         private $segnalazioni = array('scrivi' => 'scrivi_segnalazioni', 'leggi' => 'leggi_segnalazioni');
+        private $news = array('scrivi' => 'scrivi_news', 'leggi' => 'leggi_news');
         public function segnalazioni($mode){
             return $this->segnalazioni[$mode];
+        }
+        public function news($mode){
+            return $this->news[$mode];
         }
     }
 ?>
